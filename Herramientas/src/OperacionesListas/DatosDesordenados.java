43b9e9d87b3c501e689c826dd213f4eb34ce1e1c
DhaxDher <130243390+DhaxDher@users.noreply.github.com>
2023-04-22 17:52:53 -0600

    Add files via upload

---
+++ Herramientas/src/OperacionesListas/DatosDesordenados.java
@@ -0,0 +1,6 @@
+package OperacionesListas;
+
+public class DatosDesordenados {
+	//Preparar una interfaz generica que contenga las operaciones altas, bajas, modificar, imprimir, buscar, eliminar, vaciar, valida vacio
+
+}
